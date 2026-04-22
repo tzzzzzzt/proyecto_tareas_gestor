11eecf8b60bd2db7ae2c4f673266c3de874c5731
U-LAPTOP-G00QJI5R\Mauri <231790498+tzzzzzzt@users.noreply.github.com>
2026-04-21 22:18:37 -0400

    Agregado mas cosas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,21 @@
-function funcHola(){
-    let enprogreso = document.getElementById("en-progreso")
 
-    enprogeso = enprogreso+1;
-}
+const container = document.getElementById("en-progreso");
+let contarprog = 0;
+let contarcom = 0;
+
+ document.getElementById("tareaForm").addEventListener("submit", (e) => {
+        e.preventDefault();
+    let descripcion = document.getElementById("descripcion").value;
+    let prioridad = document.getElementById("prioridad").value;
+    let fecha = document.getElementById("fecha").value;
+
+    const newtarea = document.createElement("div");
+    newtarea.textContent = (descripcion+ " "+ prioridad+ " "+fecha);
+    container.appendChild(newtarea);
+    contarprog++;
+    document.getElementById('contarprog').innerText = contarprog;
+
+});
+
+
+
